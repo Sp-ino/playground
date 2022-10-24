a10bfad54bbb492c01060d97a28466645ae7e8c4
sp-ino <spinovale97@gmail.com>
2022-10-24 10:52:48 +0200

    Update playground with more examples on for loops and match statement

--- src/main.rs
+++ src/main.rs
@@ -20,21 +20,43 @@ fn main() {
     // Don't need to declare the variable I use in a for loop
     // Actually the compiler produces a warning if I declare the
     // variable i, so its scope is limited to the for loop
+    println!("Example on for loops with range");
     for i in 1..=10 {
         println!("Iteration {i}");
     }
     
+
+    // I can also assign a range to a variable and then use it
+    println!("2nd example on for loops with range");
     let range = 1..5;
     // println!("range variable is bound to {range}");
     for i in range {
         println!("i is {i}");
     }
+
+    // for loops can be used to iterate on arrays
+    println!("Example on for loops with array");
+    let arr: [i32; 6] = [1, 2, 3, 6, 1, 0];
+    let mut count: i32 = 0;
+    for val in arr {
+        count = count + 1;
+        println!("The {}-th element of arr is {}", count, val);
+    }
+
+    // But not on tuples
+    // println!("Example on for loops with tuple");
+    // let tup: (char, i8, i32, f32) = ('h', 1, 2, 9.45);
+    // for val in tup {
+    //     println!("val is {val}");
+    // }
     // -------------------------------------------------------------
 
     // -----------------match statement examples--------------------
     let m_output: i32;
     let mut outcome;
 
+    // match construct is an expression so I can use it to assign
+    // the value of a variable
     m_output = match y.cmp(&mut x) {
         Ordering::Greater => 1,
         Ordering::Equal => 0,
@@ -57,10 +79,29 @@ fn main() {
         }
     }
 
-    println!("The second match statement has bound outcome to: {outcome}")
+    println!("The second match statement has bound outcome to: {outcome}");
 
     let mut myinput = String::new();
+
+    println!("Who are you?");
+    io::stdin().read_line(&mut myinput)
+                .expect("String read failed.");
     
-    match
+    let myinput = myinput.trim();
+
+    println!("You entered {myinput}");
+    match myinput {
+        "valerio" => println!("Hello Valerio!"),
+        "luca" => println!("Hello Luca!"),
+        "gianni" => println!("Hello Gianni!"),
+        _ => println!("I don't know who you are, sorry.")
+    }
+
+    // match myinput {
+    //     String::from("valerio") => println!("Hello Valerio!"),
+    //     String::from("luca") => println!("Hello Luca!"),
+    //     String::from("gianni") => println!("Hello Gianni!"),
+    //     _ => println!("I don't know who you are, sorry.")
+    // }
     // -------------------------------------------------------------
 }
