b7697cc819682fec7d9f3f1dca7cbfca8fbffd61
sp-ino <spinovale97@gmail.com>
2022-12-28 15:55:57 +0100

    Add stuff to playground

--- src/main.rs
+++ src/main.rs
@@ -77,7 +77,7 @@ fn main() {
             println!("Default case encountered!");
             outcome = 0;
         }
-    }
+    };
 
     println!("The second match statement has bound outcome to: {outcome}");
 
@@ -88,7 +88,7 @@ fn main() {
                 .expect("String read failed.");
     
     // the .trim method removes formatting added by read_line 
-    // and converts the String to string literal. Without
+    // and converts the String (str) to string literal (which is a &str). Without
     // calling trim(), myinput would remain a string and
     // we would not be able to compare it to "valerio", "luca"
     // or "gianni" for 2 reasons:
@@ -102,13 +102,36 @@ fn main() {
         "luca" => println!("Hello Luca!"),
         "gianni" => println!("Hello Gianni!"),
         _ => println!("I don't know who you are, sorry.")
-    }
+    };
 
-    // match myinput {
-    //     String::from("valerio") => println!("Hello Valerio!"),
-    //     String::from("luca") => println!("Hello Luca!"),
-    //     String::from("gianni") => println!("Hello Gianni!"),
-    //     _ => println!("I don't know who you are, sorry.")
-    // }
+    let mut myinput2 = String::new();
+
+    println!("Please enter some string:");
+    let outcome = io::stdin()
+                                            .read_line(&mut myinput2);
+
+
+    let myinput2 = myinput2.trim();
+    match outcome {
+        Err(msg) => println!("{msg}"),
+        Ok(nchars) => println!("{} characters were read successfully, the string is {}", nchars, myinput2),
+    };
     // -------------------------------------------------------------
+
+    // -----------------Vectors-------------------------------------
+    let mut v: Vec<i32> = vec![1, 3, 2, 12, 4];
+
+    println!("v is {}", v[2]);
+    let a = v[2];
+    v[2] = v[2]+80;
+
+    v.push(100);
+    
+    // let vslice = &v[2..4];
+    println!("a is {a}");
+
+    let s = String::from("ciao");
+    println!("s slice is {}", &s[1..3]);
+    // let sslice = &v[1..3];
+    // --------------------------------------------------------------
 }
